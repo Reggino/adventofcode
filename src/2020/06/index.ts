a5464627bfa7d0117e3271d0d213b04a7b485cbc
Tim de Koning <git@king-management.nl>
2020-12-19 23:10:57 +0100

    2020 - 6 - 2

--- src/2020/06/index.ts
+++ src/2020/06/index.ts
@@ -1,12 +1,14 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
+const groupsLines = readFileSync(join(__dirname, "./input.txt"), {
+  encoding: "utf-8"
+})
+  .trim()
+  .split("\n\n");
+
 console.log(
-  readFileSync(join(__dirname, "./input.txt"), {
-    encoding: "utf-8"
-  })
-    .trim()
-    .split("\n\n")
+  groupsLines
     .map(groupLines =>
       groupLines
         .trim()
@@ -21,4 +23,23 @@ console.log(
         )
     )
     .reduce((prev, groupAnswers) => prev + Object.keys(groupAnswers).length, 0)
+);
+
+console.log(
+  groupsLines
+    .map(groupLines => {
+      const personLines = groupLines.trim().split("\n");
+      const answers = personLines.reduce<{ [answer: string]: number }>(
+        (groupPrev, personLine) =>
+          personLine.split("").reduce((personPrev, personAnswer) => {
+            groupPrev[personAnswer] = (groupPrev[personAnswer] || 0) + 1;
+            return groupPrev;
+          }, groupPrev),
+        {}
+      );
+      return Object.keys(answers).filter(
+        answer => answers[answer] === personLines.length
+      ).length;
+    })
+    .reduce((prev, answerCount) => prev + answerCount, 0)
 );
