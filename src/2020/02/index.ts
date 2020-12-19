e3b268f460585c2c0fc7085574213671c97498c0
Tim de Koning <git@king-management.nl>
2020-12-19 14:40:40 +0100

    2020 - 2 - 1

---
+++ src/2020/02/index.ts
@@ -0,0 +1,35 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+
+interface ILineData {
+  from: number;
+  to: number;
+  letter: string;
+  password: string;
+}
+
+const values = readFileSync(join(__dirname, "./input.txt"), {
+  encoding: "utf-8"
+})
+  .trim()
+  .split("\n")
+  .map(line => {
+    const matches: any = /(\d+)-(\d+) (.): (.+)/.exec(line);
+    return {
+      from: parseInt(matches[1], 10),
+      to: parseInt(matches[2], 10),
+      letter: matches[3],
+      password: matches[4]
+    };
+  }) as ILineData[];
+
+console.log(
+  values.reduce((prev, lineData) => {
+    const letterCount = (
+      lineData.password.match(new RegExp(lineData.letter, "g")) || []
+    ).length;
+    return letterCount >= lineData.from && letterCount <= lineData.to
+      ? prev + 1
+      : prev;
+  }, 0)
+);
