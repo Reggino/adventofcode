15eaa401fd5317168a27c18e0189dbf6b659b7b3
Tim de Koning <git@king-management.nl>
2021-12-26 13:28:33 +0100

    2021 - 7b

--- src/2021/07/index.ts
+++ src/2021/07/index.ts
@@ -1,23 +1,43 @@
-import {readFileSync} from "fs";
-import {join} from "path";
+import { readFileSync } from "fs";
+import { join } from "path";
 
 const positions = readFileSync(join(__dirname, "./input.txt"), {
-    encoding: "utf-8"
+  encoding: "utf-8"
 })
-    .trim()
-    .split(",")
-    .reduce<number[]>(
-        (prev, position) => [...prev, parseInt(position, 10)],
-        []
-    );
+  .trim()
+  .split(",")
+  .reduce<number[]>((prev, position) => [...prev, parseInt(position, 10)], []);
 
+// 7a
 let lowGasUsage = Number.MAX_SAFE_INTEGER;
+
 for (let i = 0; i < Math.max(...positions); i++) {
-    const gasUsage = positions.reduce((prev, position) => prev + Math.abs(position - i), 0)
-    if (gasUsage < lowGasUsage) {
-        lowGasUsage = gasUsage;
-    }
+  lowGasUsage = Math.min(
+    lowGasUsage,
+    positions.reduce((prev, position) => prev + Math.abs(position - i), 0)
+  );
 }
 
-console.log(lowGasUsage)
+console.log(lowGasUsage);
 
+// 7b
+function getGasUsage(distance: number): number {
+  let gasUsage = 0;
+  for (let i = 1; i <= distance; i++) {
+    gasUsage += i;
+  }
+  return gasUsage;
+}
+
+let lowTotalGasUsage = Number.MAX_SAFE_INTEGER;
+for (let i = 0; i < Math.max(...positions); i++) {
+  lowTotalGasUsage = Math.min(
+    lowTotalGasUsage,
+    positions.reduce(
+      (prev, position) => prev + getGasUsage(Math.abs(position - i)),
+      0
+    )
+  );
+}
+
+console.log(lowTotalGasUsage);
