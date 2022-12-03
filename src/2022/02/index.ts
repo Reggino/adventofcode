e86bf2f5db30214bcc75e19aa8d23bebf1079fba
Tim de Koning <git@king-management.nl>
2022-12-03 12:41:18 +0100

    2022 - 2 - 2

--- src/2022/02/index.ts
+++ src/2022/02/index.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import { join } from "path";
-import { sum } from "lodash";
+import { startsWith, sum } from "lodash";
 
 enum EOutcome {
   WIN,
@@ -8,49 +8,59 @@ enum EOutcome {
   DRAW
 }
 
+enum ERockPaperScissor {
+  ROCK = "A",
+  PAPER = "B",
+  SCISSORS = "C"
+}
+
+enum EStrategy {
+  X = "X",
+  Y = "Y",
+  Z = "Z"
+}
+
 const values = readFileSync(join(__dirname, "./input.txt"), {
   encoding: "utf-8"
 })
   .trim()
   .split("\n")
-  .map(row => row.split(" ")) as ["A" | "B" | "C", "X" | "Y" | "Z"][];
+  .map(row => row.split(" ")) as [ERockPaperScissor, EStrategy][];
 
-const scores = values.map(([request, answer]) => {
-  let score = 0;
-  let outcome: EOutcome = EOutcome.DRAW;
+const answerValues = {
+  A: 1,
+  B: 2,
+  C: 3
+};
 
-  switch (answer) {
-    case "X":
-      score += 1;
-      if (request === "B") {
-        outcome = EOutcome.LOSE;
-      }
-      if (request === "C") {
-        outcome = EOutcome.WIN;
-      }
-      break;
+const strategyMap = {
+  X: ERockPaperScissor.ROCK,
+  Y: ERockPaperScissor.PAPER,
+  Z: ERockPaperScissor.SCISSORS
+};
 
-    case "Y":
-      score += 2;
-      if (request === "A") {
-        outcome = EOutcome.WIN;
-      }
-      if (request === "C") {
-        outcome = EOutcome.LOSE;
-      }
-      break;
-
-    case "Z":
-      score += 3;
-      if (request === "A") {
-        outcome = EOutcome.LOSE;
-      }
-      if (request === "B") {
-        outcome = EOutcome.WIN;
-      }
-      break;
+const outcomes = {
+  A: {
+    A: EOutcome.DRAW,
+    B: EOutcome.WIN,
+    C: EOutcome.LOSE
+  },
+  B: {
+    A: EOutcome.LOSE,
+    B: EOutcome.DRAW,
+    C: EOutcome.WIN
+  },
+  C: {
+    A: EOutcome.WIN,
+    B: EOutcome.LOSE,
+    C: EOutcome.DRAW
   }
+};
 
+const scores = values.map(([request, strategy]) => {
+  const answer = strategyMap[strategy];
+  let score = answerValues[answer];
+  let outcome = outcomes[request][answer];
   if (outcome === EOutcome.DRAW) {
     score += 3;
   }
@@ -60,5 +70,40 @@ const scores = values.map(([request, answer]) => {
   return score;
 });
 
-// 12196 is too high
+// 11841
 console.log(sum(scores));
+
+const strategyScores = values.map(([request, strategy]) => {
+  let answer: ERockPaperScissor | undefined = undefined;
+
+  Object.entries(outcomes).forEach(([req, requestOutcomes]) => {
+    if (req !== request) {
+      return;
+    }
+    Object.entries(requestOutcomes).forEach(([response, outcome]) => {
+      if (
+        (strategy === EStrategy.X && outcome === EOutcome.LOSE) ||
+        (strategy === EStrategy.Y && outcome === EOutcome.DRAW) ||
+        (strategy === EStrategy.Z && outcome === EOutcome.WIN)
+      ) {
+        answer = response as ERockPaperScissor;
+      }
+    });
+  });
+
+  if (!answer) {
+    throw new Error("Could not find answer");
+  }
+  let score: number = answerValues[answer];
+  let outcome = outcomes[request][answer];
+  if (outcome === EOutcome.DRAW) {
+    score += 3;
+  }
+  if (outcome === EOutcome.WIN) {
+    score += 6;
+  }
+  return score;
+});
+
+// 13022
+console.log(sum(strategyScores));
