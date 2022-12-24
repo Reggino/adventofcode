9a91a624274d55550b2a21b2c2ed70051c1b1e14
Tim de Koning <git@king-management.nl>
2022-12-24 13:43:03 +0100

    2022 - 6

---
+++ src/2022/06/1.ts
@@ -0,0 +1,21 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { uniq } from "lodash";
+
+const stream = readFileSync(join(__dirname, "./input.txt"), {
+  encoding: "utf-8"
+}).trim();
+
+const START_MARKER_LENGTH = 4;
+let pointer = 0;
+let allLettersDiffer = false;
+
+while (!allLettersDiffer) {
+  pointer++;
+  const letters = stream
+    .substring(pointer, pointer + START_MARKER_LENGTH)
+    .split("");
+  allLettersDiffer = uniq(letters).length === START_MARKER_LENGTH;
+}
+
+console.log(pointer + START_MARKER_LENGTH);
