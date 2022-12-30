3e5337f32d9d96b09080e042c445b7a596ea0398
Tim de Koning <git@king-management.nl>
2022-12-30 21:59:08 +0100

    2022 - 9 - 1

--- src/2022/09/index.ts
+++ src/2022/09/index.ts
@@ -1,7 +1,9 @@
 import { readFileSync } from "fs";
 import { join } from "path";
 
-const instructions = readFileSync(join(__dirname, "./sample.txt"), {
+const DEBUG = 1;
+
+const instructions = readFileSync(join(__dirname, "./input.txt"), {
   encoding: "utf-8"
 })
   .trim()
@@ -32,12 +34,15 @@ const state = {
 function render() {
   return new Promise<void>(resolve => {
     setTimeout(() => {
-      console.log("\x1Bc");
       // console.log(state);
       state.minX = Math.min(state.H.x, state.minX);
       state.maxX = Math.max(state.H.x, state.maxX);
       state.minY = Math.min(state.H.y, state.minY);
       state.maxY = Math.max(state.H.y, state.maxY);
+      if (!DEBUG) {
+        return;
+      }
+      console.log("\x1Bc");
       for (let y = state.minY; y <= state.maxY; y++) {
         for (let x = state.minX; x <= state.maxX; x++) {
           if (state.H.x === x && state.H.y === y) {
@@ -57,7 +62,7 @@ function render() {
         process.stdout.write("\n");
       }
       resolve();
-    }, 20);
+    }, DEBUG);
   });
 }
 
@@ -74,6 +79,8 @@ function placeH(instruction: "U" | "D" | "L" | "R") {
   }
 }
 
+const tSpots: { [pos: string]: true } = {};
+
 function placeT() {
   // make tail follow
   const dx = state["T"]["x"] - state["H"]["x"];
@@ -81,22 +88,20 @@ function placeT() {
   if (dx < -1) {
     state["T"]["x"] += 1;
     state["T"]["y"] = state["H"]["y"];
-    return;
   }
   if (dx > 1) {
     state["T"]["x"] -= 1;
     state["T"]["y"] = state["H"]["y"];
-    return;
   }
   if (dy < -1) {
     state["T"]["x"] = state["H"]["x"];
     state["T"]["y"] += 1;
-    return;
   }
   if (dy > 1) {
     state["T"]["x"] = state["H"]["x"];
     state["T"]["y"] -= 1;
   }
+  tSpots[`${state["T"]["x"]}_${state["T"]["y"]}`] = true;
 }
 
 async function main() {
@@ -113,4 +118,4 @@ async function main() {
 
 render()
   .then(main)
-  .then(() => console.log("done"));
+  .then(() => console.log(Object.keys(tSpots).length));
