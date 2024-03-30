c150c039d9b0eea87d6424cb64cdaaf81b9c9ee5
Tim de Koning <git@king-management.nl>
2024-03-30 18:41:27 +0100

    2023 - 4 - 1

---
+++ src/2023/04/1.ts
@@ -0,0 +1,31 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { difference, intersection } from "lodash";
+
+interface ICard {
+  winning: number[];
+  numbers: number[];
+  cardId: string;
+}
+
+const cards: ICard[] = readFileSync(join(__dirname, "./input.txt"), {
+  encoding: "utf-8"
+})
+  .trim()
+  .split("\n")
+  .map(line => {
+    const [cardId, data] = line.trim().split(": ");
+    const [winning, numbers] = data.trim().split(" | ");
+    return {
+      cardId,
+      winning: winning.split(/ +/g).map(numberString => parseInt(numberString)),
+      numbers: numbers.split(/ +/g).map(numberString => parseInt(numberString))
+    };
+  });
+
+function getCardScore(card: ICard) {
+  const intersectionLength = intersection(card.winning, card.numbers).length;
+  return intersectionLength ? Math.pow(2, intersectionLength - 1) : 0;
+}
+
+console.log(cards.reduce((prev, card) => prev + getCardScore(card), 0));
