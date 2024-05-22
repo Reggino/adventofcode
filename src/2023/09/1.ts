e5ddd7a96d747fff1bb8f50bd199d57f3fcd0299
Tim de Koning <git@king-management.nl>
2024-05-22 22:39:33 +0200

    2023 - 9 - 1

---
+++ src/2023/09/1.ts
@@ -0,0 +1,44 @@
+import { readFileSync } from "fs";
+import { join } from "path";
+import { sum } from "lodash";
+
+const lines = readFileSync(join(__dirname, "./input.txt"), {
+  encoding: "utf-8",
+})
+  .trim()
+  .split("\n");
+
+const numberss = lines.map((line) =>
+  line.split(" ").map((number) => parseInt(number)),
+);
+
+const pyramids = numberss.map((numbers) => {
+  let lastRow = numbers;
+  const rows = [lastRow];
+
+  while (lastRow.findIndex((number) => number !== 0) !== -1) {
+    const newRow = [];
+    for (let i = 1; i < lastRow.length; i++) {
+      newRow.push(lastRow[i] - lastRow[i - 1]);
+    }
+    rows.push(newRow);
+    lastRow = newRow;
+  }
+  return rows;
+});
+
+const calcNextValue = (pyramid: number[][]): number => {
+  for (let rowIndex = pyramid.length - 1; rowIndex > 0; rowIndex--) {
+    const sourceRow = pyramid[rowIndex];
+    const incrementValue = sourceRow[sourceRow.length - 1];
+    const targetRow = pyramid[rowIndex - 1];
+    const targetValue = targetRow[targetRow.length - 1];
+    targetRow.push(targetValue + incrementValue);
+  }
+  return pyramid[0][pyramid[0].length - 1];
+};
+
+// 101 = too low
+1708206096;
+// 1708206166 = too high
+console.log(sum(pyramids.map(calcNextValue)));
